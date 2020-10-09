9183d4bfe708bf5a363075597de70b4373f802d6
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-10-09 14:49:24 -0400

    fixing urls/views, moving buttons to base.html

--- project/urls.py
+++ project/urls.py
@@ -23,18 +23,18 @@ from questionbox import views as questionbox_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')), # path for registration redux
-    path('', questionbox_views.landingpage, name='landingpage') # path for landing page
+    path('', questionbox_views.landingpage, name='landingpage'), # path for landing page
 
     # questions
-    path('questionbox/question_list', questionbox_views.question_list, name='question_list') # question list
-    path('questionbox/question_create', questionbox_views.question_create, name='question_create') # question create/ask
-    path('questionbox/question_search', questionbox_views.question_search, name='question_search') # question search
+    path('questionbox/question_list', questionbox_views.question_list, name='question_list'), # question list
+    path('questionbox/question_create', questionbox_views.question_create, name='question_create'), # question create/ask
+    path('questionbox/question_search', questionbox_views.question_search, name='question_search'), # question search
 
     # answers
 
-    path('questionbox/answer_list', questionbox_views.answer_list, name='answer_list') # answer list
-    path('questionbox/answer_create', questionbox_views.answer_create, name='answer_create') # answer create
-    path('questionbox/answer_search', questionbox_views.answer_search, name='answer_search') # answer search
+    path('questionbox/answer_list', questionbox_views.answer_list, name='answer_list'), # answer list
+    path('questionbox/answer_create', questionbox_views.answer_create, name='answer_create'), # answer create
+    path('questionbox/answer_search', questionbox_views.answer_search, name='answer_search'), # answer search
 
     # should login path go to http://127.0.0.1:8000/accounts/login/?
 
--- questionbox/views.py
+++ questionbox/views.py
@@ -13,3 +13,23 @@ def landingpage(request):
 def question_list(request):
     question=Question.objects.all()
     return render (request, 'question/question_list.html')
+
+def question_create(request):
+    question=Question.objects.all()
+    return render (request, 'question/question_create.html')
+
+def question_search(request):
+    question=Question.objects.all()
+    return render (request, 'question/question_search.html')
+
+def answer_list(request):
+    answer=Answer.objects.all()
+    return render (request, 'answer/answer_list.html')
+
+def answer_create(request):
+    answer=Answer.objects.all()
+    return render (request, 'answer/answer_create.html')
+
+def answer_search(request):
+    answer=Answer.objects.all()
+    return render (request, 'answer/answer_search.html')
