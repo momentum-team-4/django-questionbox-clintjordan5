5814ba7e7009042d6bc0d68e4891fc0cd3f44f2e
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-10-15 11:00:21 -0400

    added rest framework and API app. working on serializer/models

--- project/settings.py
+++ project/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    # added rest framework
 
     # Third-party
     'debug_toolbar',
